fac7602ebeced6eda41fe629ad7178776f4e5d23
johancass <johancass38@gmail.com>
2024-09-18 20:12:41 -0600

    Se agrego el calculo de la edad del animal en cuestion y se cambio el color de la letra a white

--- app/src/main/java/com/example/edadcanina/MainActivity.java
+++ app/src/main/java/com/example/edadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText ageEdit = findViewById(R.id.age_id);
+        TextView resultText = findViewById(R.id.result_text);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String age = ageEdit.getText().toString();
+                int ageInt = Integer.parseInt(age);
+                int result = ageInt * 7;
+                String resultsString = "La edad del canino es: " + result + " años";
+                resultText.setText(resultsString);
+            }
         });
     }
 }
